e691373c4f1f861ea0e149ce4e65451315aeac2c
Sidakveer <sidakveersingh8@gmail.com>
2022-09-16 13:43:11 -0400

    create a new flask form to leave a new rating and review for a given movie card

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
@@ -10,26 +9,24 @@ import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap(app)
-
-##CREATE DB
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
-##CREATE TABLE
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
+    title = db.Column(db.String(120), unique=True, nullable=False)
     year = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String(500), nullable=False)
-    rating = db.Column(db.Float, nullable=True)
-    ranking = db.Column(db.Integer, nullable=True)
-    review = db.Column(db.String(250), nullable=True)
-    img_url = db.Column(db.String(250), nullable=False)
-db.create_all()
+    description = db.Column(db.String(120), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(120), nullable=False)
+    img_url = db.Column(db.String(120), unique=True, nullable=False)
 
+    def __repr__(self):
+        return f'<Movie {self.title}>'
 
+# db.create_all()
 # new_movie = Movie(
 #     title="Phone Booth",
 #     year=2002,
@@ -37,16 +34,32 @@ db.create_all()
 #     rating=7.3,
 #     ranking=10,
 #     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#     img_url=""
 # )
 # db.session.add(new_movie)
 # db.session.commit()
-
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10!")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
 
 @app.route("/")
 def home():
-    all_movies = Movie.query.all()
-    return render_template("index.html", movies=all_movies)
+    movies = db.session.query(Movie).all()
+    return render_template("index.html", movies=movies)
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def rate_movie():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", movie=movie, form=form)
 
 
 if __name__ == '__main__':
